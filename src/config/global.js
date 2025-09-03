c0c03edf9643afa89e113122b3a8f944cd9e9d05
Leyson Castaño <leysofcp@hotmail.com>
2025-09-02 20:58:21 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -149,7 +149,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/SIE_CF01_DU.zip',
       },
       {
         icono: 'fas fa-download',
